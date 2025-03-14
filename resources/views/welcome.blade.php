dcb655b187a91ecbea9e176a75ec994ebe0f1b57
meshack <adeoyemeshack37@gmail.com>
2025-03-14 15:59:31 +0100

    add name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -191,8 +191,8 @@
                             <img class="position-absolute w-100 h-100" src="{{ asset('images/j1.jpg') }}" style="object-fit: cover;">
                             <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                                 <div class="p-3" style="max-width: 700px;">
-                                    <h1 class="display-4 text-white mb-3 animate__animated animate__fadeInDown">Men Fashion</h1>
-                                    <p class="mx-md-5 px-5 animate__animated animate__bounceIn">Lorem rebum magna amet lorem magna erat diam stet. Sadips duo stet amet amet ndiam elitr ipsum diam</p>
+                                    <h1 class="display-4 text-white mb-3 animate__animated animate__fadeInDown"></h1>
+                                    <p class="mx-md-5 px-5 animate__animated animate__bounceIn"></p>
                                     <a class="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp" href="#">Shop Now</a>
                                 </div>
                             </div>
